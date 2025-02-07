f327beabd150300282d7fe4b90a4c25870f80d3e
Bharath Ram Manoharan <bharathram.manoharan@salesforce.com>
2025-02-06 21:47:44 -0800

    first commit

---
+++ SpringAPI/src/main/java/org/example/springapi/api/controller/UserController.java
@@ -0,0 +1,26 @@
+package org.example.springapi.api.controller;
+
+import org.example.springapi.api.model.User;
+import org.example.springapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id){
+        Optional<User> user = userService.getUser(id);
+        return (User) user.orElse(null);
+    }
+}
